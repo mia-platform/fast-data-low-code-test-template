a08343075a3be733f7c975e746d1e54ea0f4b30a
Giorgio Acquati <giorgio.acquati@mia-platform.eu>
2022-03-24 14:54:51 +0100

    Convert project to Typescript

---
+++ configurations/validator.js
@@ -0,0 +1,1 @@
+module.exports = (logger, singleView) => typeof singleView.taxCode === 'string'
---
+++ tests/validator/invalid/index.ts
@@ -0,0 +1,8 @@
+import noTaxCode from "./noTaxCode";
+
+export default [
+    {
+        name: 'no tax code',
+        sv: noTaxCode
+    }
+] as const
